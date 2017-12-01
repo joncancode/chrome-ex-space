e9745a78a72a3908facae945ef7ba1475e31b838
joncancode <jc.haines19@gmail.com>
2017-11-30 23:57:28 -0500

    initial setup of axios

---
+++ popup.js
@@ -0,0 +1,16 @@
+// alert('test')
+
+var apiUrl = "https://github.com/r-spacex/SpaceX-API/wiki/Upcoming-Launches";
+
+function getSpaceData() {
+    axios.get(apiUrl)
+    .then(function (response) {
+        spaceData.innerHTML = response.data.forms[0].name;
+    })
+    .catch(function (error) {
+        spaceData.innerHTML = "(An error has occurred.)";
+    });
+}
+
+var button = document.querySelector(".spaceButton");
+button.addEventListener("click", getSpaceData);
